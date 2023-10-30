6515e03732bd8299d66132a24109bf797d2b4e4d
lake <102535481@qq.com>
2023-10-30 15:57:11 +0800

    init

--- app/service/qrcode_service_test.go
+++ app/service/qrcode_service_test.go
@@ -1,9 +1,18 @@
 package service
 
 import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"github.com/chromedp/cdproto/network"
+	"github.com/chromedp/cdproto/runtime"
+	"github.com/chromedp/chromedp"
+	"github.com/duke-git/lancet/v2/fileutil"
 	assert2 "github.com/stretchr/testify/assert"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 	"testing"
 )
 
@@ -35,5 +44,73 @@ func TestQrcodeService_Parser(t *testing.T) {
 	url, err := qrcodeService.Parser(qrcodePath)
 	assert.NoError(err)
 	assert.Equal(*url, "https://baidu.com")
-
+}
+func TestQrodeServie_Parser2(t *testing.T) {
+	assert := assert2.New(t)
+	qrcodeService := NewQrcodeService()
+	url, err := qrcodeService.Parser("./file/aa.png")
+	assert.NoError(err)
+	fmt.Println(*url)
+}
+func TestQrcodeService_QrcodeGet(t *testing.T) {
+	assert := assert2.New(t)
+	qrcodeService := NewQrcodeService()
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", true),
+		chromedp.Flag("disable-gpu", true),
+		chromedp.Flag("no-sandbox", true),
+		chromedp.Flag("disable-dev-shm-usage", true),
+	)
+	allocExt, cancel := chromedp.NewExecAllocator(ctx, opts...)
+	defer cancel()
+	ctx, cancel = chromedp.NewContext(allocExt)
+	defer cancel()
+	js := `new Promise((resolve, reject) => {
+				setTimeout(async () => {
+					let response = await fetch('https://mp.weixin.qq.com/cgi-bin/scanloginqrcode?action=getqrcode&random='+new Date().getTime())
+					let blob = await response.blob()
+					let reader = new FileReader()
+					reader.readAsDataURL(blob)
+					reader.onloadend = function () {
+						let base64data = reader.result
+						resolve(base64data)
+					}
+					reader.onerror = function () {
+						reject('blob转base64失败，请联系开发人员')
+					}
+				}, 1000); // 模拟异步操作
+			});
+	`
+	var result string
+	err := chromedp.Run(
+		ctx,
+		chromedp.Navigate("https://mp.weixin.qq.com"),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			cookies, err := network.GetCookies().Do(ctx)
+			if err != nil {
+				panic(err)
+			}
+			for _, cookie := range cookies {
+				println(cookie.Name + ":" + cookie.Value)
+			}
+			return nil
+		}),
+		chromedp.Evaluate(js, &result, func(ep *runtime.EvaluateParams) *runtime.EvaluateParams {
+			return ep.WithAwaitPromise(true)
+		}),
+	)
+	regexImage := regexp.MustCompile(`^data:image/(jpg|png);base64,`)
+	//正则替换jpg或者png
+	arrayBufferBytes, err := base64.StdEncoding.DecodeString(regexImage.ReplaceAllString(result, ""))
+	if err != nil {
+		panic(err)
+	}
+	fileutil.WriteBytesToFile("./qrcode.png", arrayBufferBytes)
+	defer os.Remove("./qrcode.png")
+	context, err := qrcodeService.Parser("./qrcode.png")
+	assert.NoError(err)
+	assert.NotEmptyf(context, "context is empty")
+	assert.Equal(strings.HasPrefix(*context, "http://mp.weixin.qq.com"), true)
 }
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"github.com/chromedp/cdproto/network"
+	"github.com/chromedp/cdproto/runtime"
 	"github.com/chromedp/chromedp"
 	"github.com/duke-git/lancet/v2/fileutil"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -13,7 +17,7 @@ func main() {
 	defer cancel()
 
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", true),
+		chromedp.Flag("headless", false),
 		chromedp.Flag("disable-gpu", true),
 		chromedp.Flag("no-sandbox", true),
 		chromedp.Flag("disable-dev-shm-usage", true),
@@ -24,10 +28,27 @@ func main() {
 
 	ctx, cancel = chromedp.NewContext(allocExt)
 	defer cancel()
+	//qrcodeService := service.NewQrcodeService()
 
+	js := `new Promise((resolve, reject) => {
+				setTimeout(async () => {
+					let response = await fetch('https://mp.weixin.qq.com/cgi-bin/scanloginqrcode?action=getqrcode&random=1698639598411')
+					let blob = await response.blob()
+					let reader = new FileReader()
+					reader.readAsDataURL(blob)
+					reader.onloadend = function () {
+						let base64data = reader.result
+						resolve(base64data)
+					}
+				}, 1000); // 模拟异步操作
+			});
+	`
+	//qrcodeJs := `(await fetch('https://mp.weixin.qq.com/cgi-bin/scanloginqrcode?action=getqrcode&random=1698639598411')).text()`
+	//var qrcodeUrl *string
+	var result string
 	err := chromedp.Run(
 		ctx,
-		chromedp.Navigate("https://baidu.com"),
+		chromedp.Navigate("https://mp.weixin.qq.com"),
 		chromedp.ActionFunc(func(ctx context.Context) error {
 			cookies, err := network.GetCookies().Do(ctx)
 			if err != nil {
@@ -38,16 +59,46 @@ func main() {
 			}
 			return nil
 		}),
-		//截图
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			var buf []byte
-			if err := chromedp.CaptureScreenshot(&buf).Do(ctx); err != nil {
-				return err
-			}
-			err := fileutil.WriteBytesToFile("baidu.png", buf)
-			return err
+		chromedp.Evaluate(js, &result, func(ep *runtime.EvaluateParams) *runtime.EvaluateParams {
+			return ep.WithAwaitPromise(true)
 		}),
+		//截图
+		//chromedp.ActionFunc(func(ctx context.Context) error {
+		//	return retry.Retry(func() error {
+		//		log := middleware.Logger
+		//		var buf []byte
+		//		if err := chromedp.Screenshot("img.login__type__container__scan__qrcode", &buf, chromedp.NodeVisible).Do(ctx); err != nil {
+		//			log.Errorf("chromedp.Screenshot err: %s", err.Error())
+		//			return err
+		//		}
+		//		err := fileutil.WriteBytesToFile("./qrcode.png", buf)
+		//		if err != nil {
+		//			log.Errorf("fileutil.WriteBytesToFile err: %s", err.Error())
+		//			return err
+		//		}
+		//		defer os.Remove("./qrcode.png")
+		//		qrcodeUrl, err := qrcodeService.Parser("./qrcode.png")
+		//		if err != nil {
+		//			log.Errorf("qrcodeService.Parser err: %s", err.Error())
+		//			return err
+		//		}
+		//		if qrcodeUrl != nil && *qrcodeUrl != "" {
+		//			log.Errorf("qrcodeUrl: %s", *qrcodeUrl)
+		//			return nil
+		//		}
+		//		return errors.New("qrcode not found")
+		//	}, retry.RetryTimes(3))
+		//}),
 	)
+	arrayBufferBytes, err := base64.StdEncoding.DecodeString(strings.Replace(result, "data:image/jpg;base64,", "", -1))
+	if err != nil {
+		panic(err)
+	}
+	fileutil.WriteBytesToFile("./qrcode.png", arrayBufferBytes)
+	//fmt.Println(strings.Replace(result, "data:image/jpg;base64,", "", -1))
+	//middleware.Logger.Infof("qrcodeUrl: %s", *qrcodeUrl)
+
+	time.Sleep(10 * time.Minute)
 	if err != nil {
 		panic(err)
 	}
